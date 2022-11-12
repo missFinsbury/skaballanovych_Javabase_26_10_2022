220db71a910d445c8805d9b7dbb47e8f54162531
Mariia Skaballanovych <mashunya1@gmail.com>
2022-11-12 19:25:24 +0200

    fixed smth

--- src/main/java/com/hillel/skaballanovych/lessons/lesson3/main.java
+++ src/main/java/com/hillel/skaballanovych/lessons/lesson3/main.java
@@ -1,7 +1,11 @@
 package com.hillel.skaballanovych.lessons.lesson3;
 
 public class main {
-    public static void main(String[] args)
+    public static void main(String[] args) {
+        System.out.println("Lesson 3");
+        int age  = 10;
+
+    }
 
 }
 
