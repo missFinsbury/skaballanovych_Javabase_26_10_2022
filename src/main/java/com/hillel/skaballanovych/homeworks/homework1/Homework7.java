58996ef3b5754df2e89278a447617a61b2ade4ed
mariia Skaballanovych <mashunya1@gmail.com>
2022-12-10 18:45:48 +0200

    ghj

--- src/main/java/com/hillel/skaballanovych/homeworks/homework1/Homework7.java
+++ src/main/java/com/hillel/skaballanovych/homeworks/homework1/Homework7.java
@@ -4,64 +4,43 @@ import java.util.Scanner;
 
 public class Homework7 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
+        System.out.println("Guess number from 0 to 10 with 3 attempts");
+        Scanner scanner1 = new Scanner(System.in);
         int x = (int) (Math.random() * 11);
+        int try1 = scanner1.nextInt();
 
-
-            while (true) {
-                System.out.println("Guess number from 0 to 10 with 3 attempts");
-                int try1 = scanner.nextInt();
-                int i = 3;
-                if (try1 == x) {
-                    System.out.println("Win!!");
-                    break;
-
-                } else if (try1 < x) {
-                    System.out.println("Number is bigger");
-                } else if (try1 > 10) {
-                    System.out.println("Wrong data");
-                } else {
-                    System.out.println("Number is smaller");
-                }
-                scanner.nextLine();
-
-                int try2 = scanner.nextInt();
-                if (try2 == x) {
-                    System.out.println("Win!!");
-                    break;
-                } else if (try2 < x) {
-                    System.out.println("Number is bigger");
-                } else if (try2 > 10) {
-                    System.out.println("Wrong data");
-                } else {
-                    System.out.println("Number is smaller");
-                }
-
-                scanner.nextLine();
-
-                int try3 = scanner.nextInt();
-                if (try3 == x) {
-                    System.out.println("Win!!");
-                    break;
-                } else if (try3 < x) {
-                    System.out.println("Number is bigger");
-                } else if (try3 > 10) {
-                    System.out.println("Wrong data");
-                } else {
-                    System.out.println("Number is smaller");
-                }
-
+        for (int iterator = 3; iterator <= 3; iterator--) {
+            if ( iterator == 0) {
                 System.out.println("Bad beat");
-                scanner.nextLine();
                 break;
 
+            } else if (try1 == x) {
+                System.out.println("Win!!");
+                break;
 
+            } else if (try1 < x) {
+                System.out.println("Number is bigger");
+                scanner1.nextLine();
+
+            } else if (try1 > 10) {
+                System.out.println("Wrong data");
+                scanner1.nextLine();
+
+            } else {
+                System.out.println("Number is smaller");
+                scanner1.nextLine();
             }
 
         }
 
+        }
+
     }
+
+
+
+
+
 
 
 
